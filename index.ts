684dbab018d7574eb7d8f3478d1db4e70a0eed6f
ghoshshreya <shrghosh08@gmail.com>
2023-08-03 14:24:34 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -24,6 +24,7 @@ source.subscribe({
   complete: () => console.log('Completed'),
 });
 
+// Error Scenario
 let _source = new Observable((observer) => {
   observer.next(1);
   observer.error(2);
@@ -36,25 +37,25 @@ _source.subscribe({
   complete: () => console.log('Completed'),
 });
 
-// /*----------1. Using of----------*/
-// let source1 = of([1, 2, 3]);
-// source1.subscribe((x) => console.log(x));
+/*----------1. Using of----------*/
+let source1 = of([1, 2, 3]);
+source1.subscribe((x) => console.log(x));
 
-// let source2 = of('Hello World');
-// source2.subscribe((x) => console.log(x));
+let source2 = of('Hello World');
+source2.subscribe((x) => console.log(x));
 
-// /*----------2. Using from----------*/
-// let source3 = from([1, 2, 3]);
-// source3.subscribe((x) => console.log(x));
+/*----------2. Using from----------*/
+let source3 = from([1, 2, 3]);
+source3.subscribe((x) => console.log(x));
 
-// let source4 = from('Hello World');
-// source4.subscribe((x) => console.log(x));
+let source4 = from('Hello World');
+source4.subscribe((x) => console.log(x));
 
-// /*----------3. Using interval operator----------*/
-// let source5 = interval(1000).pipe(take(5));
-// source5.subscribe((x) => console.log(x));
+/*----------3. Using interval operator----------*/
+let source5 = interval(1000).pipe(take(5));
+source5.subscribe((x) => console.log(x));
 
-// /*----------4. Using fromEvent operator----------*/
-// let btn = document.querySelector('#my-btn');
-// let source6 = fromEvent(btn, 'click');
-// source6.subscribe((x) => console.log('clicked'));
+/*----------4. Using fromEvent operator----------*/
+let btn = document.querySelector('#my-btn');
+let source6 = fromEvent(btn, 'click');
+source6.subscribe((x) => console.log('clicked'));
